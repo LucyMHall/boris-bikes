beea0ae18fd18f21f496b79eb20ce779fafd3814
LucyMHall <lucymhall1995@gmail.com>
2019-05-29 16:32:34 +0100

    One line RSPEC syntax

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,7 +1,5 @@
 require "dockingstation"
 
 describe Dockingstation do
-  it "releases bike" do
-    expect(Dockingstation.new).to respond_to(:release_bike)
-  end
+  it { is_expected.to respond_to :release_bike }
 end
