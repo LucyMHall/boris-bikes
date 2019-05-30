e4f44510ad016c56503df20088ab0439d353c9b5
LucyMHall <lucymhall1995@gmail.com>
2019-05-30 16:23:51 +0100

    Error message if calling release_bike when no bike

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -3,12 +3,19 @@ require_relative "bike"
 class Dockingstation
 attr_reader :dockingstationcontents
 
+  def initialize
+    @dockingstationcontents = []
+  end
+
   def release_bike
-     Bike.new
+    if @dockingstationcontents.empty?
+      raise"No bikes"
+    else
+      Bike.new
+    end
   end
 
   def dock(bike)
-    @dockingstationcontents = []
     @dockingstationcontents << bike
   end
 
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -5,12 +5,20 @@ describe Dockingstation do
 
   it " creates an instance of the Bike class" do
     dockingstation = Dockingstation.new
+    bike = Bike.new
+    dockingstation.dock(bike)
     expect(dockingstation.release_bike).to be_a Bike
   end
+
   it "adds a bike to the dockingstation" do
     dockingstation = Dockingstation.new
     bike = Bike.new
     dockingstation.dock(bike)
     expect(dockingstation.dockingstationcontents).to include(bike)
+  end
+
+  it "checks if itself is empty" do
+    dockingstation = Dockingstation.new
+    expect{dockingstation.release_bike}.to raise_error("No bikes")
   end
 end
