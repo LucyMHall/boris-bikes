ba28f53033e58e9a7e8b9998acc93c25114e3af6
LucyMHall <lucymhall1995@gmail.com>
2019-05-30 15:49:27 +0100

    Attribute reader allows us to see contents of dockingstation

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,9 +1,16 @@
 require_relative "bike"
 
 class Dockingstation
+attr_reader :dockingstationcontents
 
   def release_bike
      Bike.new
   end
+
+  def dock(bike)
+    @dockingstationcontents = []
+    @dockingstationcontents << bike
+  end
+
 
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -7,5 +7,10 @@ describe Dockingstation do
     dockingstation = Dockingstation.new
     expect(dockingstation.release_bike).to be_a Bike
   end
-
+  it "adds a bike to the dockingstation" do
+    dockingstation = Dockingstation.new
+    bike = Bike.new
+    dockingstation.dock(bike)
+    expect(dockingstation.dockingstationcontents).to include(bike)
+  end
 end
